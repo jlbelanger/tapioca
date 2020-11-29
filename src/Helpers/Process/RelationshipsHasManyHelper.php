49a54d1df42e8c9dd2c66c37d398c0533ea6f549
Jenny Belanger <jbelangr@gmail.com>
2020-11-28 21:35:42 -0500

    Refactor updating hasMany relationships

---
+++ src/Helpers/Process/RelationshipsHasManyHelper.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Jlbelanger\LaravelJsonApi\Helpers\Process;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Arr;
+use Jlbelanger\LaravelJsonApi\Exceptions\JsonApiException;
+use Jlbelanger\LaravelJsonApi\Helpers\Input\DataHelper;
+use Jlbelanger\LaravelJsonApi\Helpers\Process\AttributesHelper;
+
+class RelationshipsHasManyHelper
+{
+	protected static $tempIdPrefix = 'temp-';
+
+	/**
+	 * Note: This is not in the JSON API spec.
+	 *
+	 * @param  array   $relData
+	 * @param  HasMany $existing
+	 * @param  string  $key
+	 * @param  Model   $record
+	 * @param  array   $included
+	 * @return array
+	 */
+	public static function update(array $relData, HasMany $existing, string $key, Model $record, array $included) : array
+	{
+		$existingIds = array_map('strval', $existing->pluck($existing->getLocalKeyName())->toArray());
+		$newIds = array_map('strval', Arr::pluck($relData['data'], 'id'));
+
+		$deleteData = self::delete($existing, $existingIds, $newIds);
+		$addData = self::add($relData, $existing, $key, $record, $included);
+
+		return [
+			'deleteIds' => $deleteData['ids'],
+			'addIds' => $addData['ids'],
+			'deleted' => $deleteData['data'],
+		];
+	}
+
+	/**
+	 * Deletes any records that are in existingIds but not newIds.
+	 *
+	 * @param  HasMany $existing
+	 * @param  array   $existingIds
+	 * @param  array   $newIds
+	 * @return array
+	 */
+	protected static function delete(HasMany $existing, array $existingIds, array $newIds) : array
+	{
+		$output = [
+			'ids' => [],
+			'data' => [],
+		];
+
+		$deleteIds = array_values(array_diff($existingIds, $newIds));
+		if (empty($deleteIds)) {
+			return $output;
+		}
+
+		// Get the models to delete.
+		$recordsToDelete = $existing->whereIn('id', $deleteIds);
+
+		// Get the attributes for the models we are deleting; we might want to do something with this data in an event, and once the models are deleted, we won't be able to access this data.
+		$attributeNames = array_merge(['id'], $recordsToDelete->getRelated()->getFillable());
+		$dataToDelete = $recordsToDelete->select($attributeNames)->getResults()->toArray();
+		$deletedData = [];
+		foreach ($dataToDelete as $data) {
+			$deletedData[$data['id']] = Arr::only($data, $attributeNames);
+		}
+
+		// Delete the models.
+		$recordsToDelete->delete();
+
+		return [
+			'ids' => $deleteIds,
+			'data' => $deletedData,
+		];
+	}
+
+	/**
+	 * @param  array   $relData
+	 * @param  HasMany $existing
+	 * @param  string  $key
+	 * @param  Model   $record
+	 * @param  array   $included
+	 * @return array
+	 */
+	protected static function add(array $relData, HasMany $existing, string $key, Model $record, array $included) : array
+	{
+		// Get the pivot model (eg. AlbumSong).
+		$pivotModel = get_class($existing->getRelated());
+		$output = [
+			'ids' => [],
+		];
+
+		foreach ($relData['data'] as $rel) {
+			if (strpos($rel['id'], self::$tempIdPrefix) !== 0) {
+				// We don't need to create a new record.
+				continue;
+			}
+
+			// Find the corresponding record in 'included'.
+			$includedData = self::find($included, $rel['id'], $rel['type']);
+			if (empty($includedData)) {
+				throw JsonApiException::generate([
+					'title' => "Record with id '" . $rel['id'] . "' and type '" . $rel['type'] . "' not found in 'included'.",
+					'source' => [
+						'pointer' => "/data/relationships/$key",
+					],
+				], 400);
+			}
+
+			// Create the new pivot model.
+			$relRecord = new $pivotModel();
+			$includedData = DataHelper::normalize($includedData, $relRecord->whitelistedAttributes(), $relRecord->whitelistedRelationships());
+			$includedData['attributes'][$record->getForeignKey()] = $record->id;
+			$includedData = AttributesHelper::convertSingularRelationships($includedData);
+			$new = $relRecord->create($includedData['attributes']);
+			$output['ids'][] = (string) $new->id;
+		}
+
+		return $output;
+	}
+
+	/**
+	 * @param  array  $included
+	 * @param  string $id
+	 * @param  string $type
+	 * @return array
+	 */
+	protected static function find(array $included, string $id, string $type) : array
+	{
+		foreach ($included as $record) {
+			if ((string) $record['id'] === (string) $id && $record['type'] === $type) {
+				return $record;
+			}
+		}
+		return [];
+	}
+}
--- src/Helpers/Process/RelationshipsHelper.php
+++ src/Helpers/Process/RelationshipsHelper.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Jlbelanger\LaravelJsonApi\Exceptions\JsonApiException;
-use Jlbelanger\LaravelJsonApi\Helpers\Input\DataHelper;
-use Jlbelanger\LaravelJsonApi\Helpers\Process\AttributesHelper;
+use Jlbelanger\LaravelJsonApi\Helpers\Process\RelationshipsHasManyHelper;
 
 class RelationshipsHelper
 {
@@ -33,7 +32,7 @@ class RelationshipsHelper
 			if ($className === 'BelongsToMany') {
 				$result = self::updateBelongsToMany($relData, $existing);
 			} elseif ($className === 'HasMany') {
-				$result = self::updateHasMany($relData, $existing, $key, $record, $included);
+				$result = RelationshipsHasManyHelper::update($relData, $existing, $key, $record, $included);
 			} else {
 				throw JsonApiException::generate([
 					'title' => "Relationship type '$className' is not supported.",
@@ -78,82 +77,5 @@ class RelationshipsHelper
 			'addIds' => $addIds,
 			'deleted' => [],
 		];
-	}
-
-	/**
-	 * Note: This is not in the JSON API spec.
-	 *
-	 * @param  array   $relData
-	 * @param  HasMany $existing
-	 * @param  string  $key
-	 * @param  Model   $record
-	 * @param  array   $included
-	 * @return array
-	 */
-	protected static function updateHasMany(array $relData, HasMany $existing, string $key, Model $record, array $included) : array
-	{
-		$existingIds = array_map('strval', $existing->pluck($existing->getLocalKeyName())->toArray());
-		$newIds = array_map('strval', Arr::pluck($relData['data'], 'id'));
-
-		$deleteIds = array_values(array_diff($existingIds, $newIds));
-		$deleteData = [];
-		if (!empty($deleteIds)) {
-			$deletable = $existing->whereIn('id', $deleteIds);
-			$attributes = array_merge(['id'], $deletable->getRelated()->getFillable());
-			$deleteTerms = $deletable->select($attributes)->getResults()->toArray();
-			foreach ($deleteTerms as $term) {
-				$deleteData[$term['id']] = Arr::only($term, $attributes);
-			}
-			$deletable->delete();
-		}
-
-		$model = get_class($existing->getRelated());
-
-		$addIds = [];
-		foreach ($relData['data'] as $rel) {
-			if (strpos($rel['id'], self::$tempIdPrefix) !== 0) {
-				continue;
-			}
-
-			$includedData = self::findIncluded($included, $rel['id'], $rel['type']);
-			if (empty($includedData)) {
-				throw JsonApiException::generate([
-					'title' => "Record with id '" . $rel['id'] . "' and type '" . $rel['type'] . "' not found in 'included'.",
-					'source' => [
-						'pointer' => "/data/relationships/$key",
-					],
-				], 400);
-			}
-
-			// TODO: Nested relationships won't work.
-			$relRecord = new $model();
-			$includedData = DataHelper::normalize($includedData, $relRecord->whitelistedAttributes(), $relRecord->whitelistedRelationships());
-			$includedData['attributes'][$record->getForeignKey()] = $record->id;
-			$includedData = AttributesHelper::convertSingularRelationships($includedData);
-			$new = $relRecord->create($includedData['attributes']);
-			$addIds[] = (string) $new->id;
-		}
-
-		return [
-			'deleteIds' => $deleteIds,
-			'addIds' => $addIds,
-			'deleted' => $deleteData,
-		];
-	}
-
-	/**
-	 * @param  array  $included
-	 * @param  string $id
-	 * @param  string $type
-	 * @return array
-	 */
-	protected static function findIncluded(array $included, string $id, string $type) : array
-	{
-		foreach ($included as $record) {
-			if ((string) $record['id'] === (string) $id && $record['type'] === $type) {
-				return $record;
-			}
-		}
-		return [];
 	}
 }
